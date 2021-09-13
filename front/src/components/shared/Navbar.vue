c4a1dc78cdb2aca71adc9125f564d3cc92c9c298
TheMentalist-w <weronika.mro@interia.pl>
2021-09-13 17:30:07 +0200

    initial take on login page design

---
+++ front/src/components/shared/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app-bar
+    app
+    color="primary"
+    dark
+  >
+    <div class="d-flex align-center">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+        transition="scale-transition"
+        width="40"
+      />
+
+      <v-toolbar-title style="font-size: 150%">PITBULL</v-toolbar-title>
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn
+      @click="$vuetify.theme.dark = !$vuetify.theme.dark"
+      text
+      rounded
+    >
+      <span class="mr-2">Dark Mode</span>
+      <v-icon>mdi-brightness-6</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+
+export default {
+  name: 'Navbar',
+  data: () => ({
+    //
+  })
+}
+</script>
--- front/src/plugins/vuetify.js
+++ front/src/plugins/vuetify.js
@@ -2,6 +2,19 @@ import Vue from 'vue'
 import Vuetify from 'vuetify/lib/framework'
 
 Vue.use(Vuetify)
-
+import colors from 'vuetify/lib/util/colors'
 export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: colors.green,
+        secondary: colors.grey.darken1,
+        accent: colors.shades.black,
+        error: colors.red.accent3,
+      },
+      dark: {
+        primary: colors.grey.darken2,
+      },
+    },
+  },
 })
