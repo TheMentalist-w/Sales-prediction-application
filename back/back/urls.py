e74cf3fdd314ff6dac237b5644479abaf3a3a9b9
szymon8576 <50836217+szymon8576@users.noreply.github.com>
2021-12-05 15:56:55 +0100

    code refactor

--- back/back/urls.py
+++ back/back/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include,path
+from rest_framework_swagger.views import get_swagger_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('stock_management/', include('stock_management.urls')),
     path('user_auth/', include('user_auth.urls')),
+    path('docs/', get_swagger_view(title='PitbullAPI')),
 ]
--- back/stock_management/urls/__init__.py
+++ back/stock_management/urls/__init__.py
@@ -1,6 +1,5 @@
 from .dataFetchingURLs import *
-from .documentationURLs import *
 from .stockManagingURLs import *
 
 
-urlpatterns = data_fetching_urlpatterns + documentation_urlpatterns + stock_managing_urlpatterns
+urlpatterns = data_fetching_urlpatterns + stock_managing_urlpatterns
