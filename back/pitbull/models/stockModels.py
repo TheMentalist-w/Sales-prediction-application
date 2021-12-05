3464723d1edc3f23c415fe0145da3bbb7796288e
szymon8576 <50836217+szymon8576@users.noreply.github.com>
2021-12-05 14:23:15 +0100

    added missing models

--- back/pitbull/models/stockModels.py
+++ back/pitbull/models/stockModels.py
@@ -19,7 +19,14 @@ class Feature(models.Model):
     name = models.CharField(max_length=50)
 
 
+class ProductPrediction(models.Model):
+    id = models.IntegerField(primary_key=True)
+    product = models.ForeignKey('Product', on_delete=models.DO_NOTHING)
+    prediction = models.ForeignKey('Prediction', on_delete=models.DO_NOTHING)
+
+
 class Prediction(models.Model):
+    id = models.IntegerField(primary_key=True)
     date = models.DateTimeField(auto_now_add=True, blank=True)
     value = models.IntegerField()
 
@@ -31,5 +38,26 @@ class Product(models.Model):
     group = models.ForeignKey(Group, on_delete=models.DO_NOTHING)
     inventory = models.IntegerField()
     features = models.ManyToManyField(Feature, through=ProductFeature)
-    predictions = models.ManyToManyField(Prediction, default=None)
+    predictions = models.ManyToManyField(Prediction, through=ProductPrediction)
     is_archived = models.BooleanField(default=False)
+
+
+class Place(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=40)
+    population = models.IntegerField()
+    avg_salary = models.FloatField()
+
+
+class Item(models.Model):
+    id = models.IntegerField(primary_key=True)
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    amount = models.IntegerField()
+
+
+class Document(models.Model):
+    id = models.IntegerField(primary_key=True)
+    type = models.IntegerField()
+    place = models.ForeignKey(Place, default=None, on_delete=models.DO_NOTHING)
+    datetime = models.DateTimeField(blank=True)
+    items = models.ManyToManyField(Item, default=None)
