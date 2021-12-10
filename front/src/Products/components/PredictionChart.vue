867023e92511736598dfa8e00668d2bc08b282b2
TheMentalist-w <weronika.mro@interia.pl>
2021-12-10 23:43:57 +0100

    move mock prediction data to backend

--- front/src/Products/components/PredictionChart.vue
+++ front/src/Products/components/PredictionChart.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <ShopFilter :key="shopKey" :filters="shops" :filtered="selectedShop" @changeShop="changeShop" />
-    <line-chart :data="chartData" :colors="['#4CAF50']"></line-chart>
+    <line-chart :key="chartKey" :data="chartData" :colors="['#4CAF50']"></line-chart>
   </div>
 </template>
 
@@ -13,15 +13,11 @@ export default {
   name: "PredictionChart",
   data() {
     return {
-      chartData: {
-        '2017-05-01': 2, '2017-05-02': 5, '2017-05-03': 2, '2017-05-04': 5, '2017-05-05': 2, '2017-05-06': 5, '2017-05-07': 2,
-        '2017-05-08': 5, '2017-05-09': 2, '2017-05-10': 5, '2017-05-11': 2, '2017-05-12': 5, '2017-05-13': 2, '2017-05-14': 5,
-        '2017-05-15': 2, '2017-05-16': 5, '2017-05-17': 2, '2017-05-18': 5, '2017-05-19': 2, '2017-05-20': 5, '2017-05-21': 2,
-        '2017-05-22': 5, '2017-05-23': 2, '2017-05-24': 5, '2017-05-25': 2, '2017-05-26': 5, '2017-05-27': 2, '2017-05-28': 5,
-        '2017-05-29': 2, '2017-05-30': 5, '2017-05-31': 2},
+      chartData: null,
       shops: [],
       selectedShop: null,
       shopKey: 0,
+      chartKey: 0,
     }
   },
   async mounted() {
@@ -52,7 +48,8 @@ export default {
       axios.get('/stock_management/product/prediction_history/', {params: params})
       .then(response => {
         //assign data to chart
-        this.chartData = response.data.history
+        this.chartData = response.data.history[0]
+        this.chartKey += 1
       })
     },
 
