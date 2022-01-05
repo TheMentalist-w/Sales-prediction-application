8c60906af5016ca38b355c2592fecccf33ad7f32
szymon8576 <50836217+szymon8576@users.noreply.github.com>
2022-01-05 23:15:48 +0100

    cronjobs (not active yet)

--- back/back/settings.py
+++ back/back/settings.py
@@ -53,6 +53,7 @@ CORS_ORIGIN_ALLOW_ALL=True   # CORS_ORIGIN_WHITELIST = ["http://localhost:8080"]
 
 INSTALLED_APPS = [
     'corsheaders',
+    'django_crontab',
     'rest_framework',
     'rest_framework_swagger',
     'user_auth',
@@ -134,6 +135,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CRONJOBS = [
+     # ('* 2 * * *', 'stock_management.views.predictionMakingViews.make_predictions')
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
